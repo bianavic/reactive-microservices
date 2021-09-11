07b2113d014a4f45260930832ad8b34893c4a4b8
bianavic <bianavic@gmail.com>
2021-09-11 14:13:22 -0300

    created a new method to return a list of customers transforming our map of customers into a list

--- src/main/kotlin/com/microservices/studies/CustomerController.kt
+++ src/main/kotlin/com/microservices/studies/CustomerController.kt
@@ -1,12 +1,23 @@
 package com.microservices.studies
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class CustomerController {
-    @RequestMapping(value = arrayOf("/customer"), method = arrayOf(RequestMethod.GET))
-    fun getCustomer() = Customer(1, "Jane Doe")
+
+    @Autowired
+    lateinit var customers : ConcurrentHashMap<Int, Customer>
+
+    @RequestMapping(value = arrayOf("/customer/{id}"), method = arrayOf(RequestMethod.GET))
+    fun getCustomer(@PathVariable id : Int) = customers[id]
+
+    // transformed map of customers into a list
+    @RequestMapping(value = arrayOf("/customers"), method = arrayOf(RequestMethod.GET))
+    fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
 
 }
