236c06c14a847ef0063e43fdef27c8c87dc9727c
bianavic <bianavic@gmail.com>
2021-09-11 14:01:15 -0300

    used a concurrent version and created a hashmap to map customers that adds it to Spring Context as a bean

--- src/main/kotlin/com/microservices/studies/Customer.kt
+++ src/main/kotlin/com/microservices/studies/Customer.kt
@@ -1,4 +1,4 @@
 package com.microservices.studies
 
-// provide equals(), hashCode(), toString()
+// provide equals(), hashCode(), toString(), componentN() functions, copy()
 data class Customer(var id: Int = 0, var name: String = "")
--- src/main/kotlin/com/microservices/studies/StudiesApplication.kt
+++ src/main/kotlin/com/microservices/studies/StudiesApplication.kt
@@ -2,9 +2,21 @@ package com.microservices.studies
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class StudiesApplication
+class StudiesApplication {
+	companion object {
+		val initialCustomers = arrayOf(Customer(1, "Jane Doe"),
+		Customer(2, "John Doe"),
+		Customer(3, "Mary Ann")
+		)
+	}
+
+	@Bean
+	fun customers() = ConcurrentHashMap<Int,Customer>(initialCustomers.associateBy(Customer::id))
+}
 
 fun main(args: Array<String>) {
 	runApplication<StudiesApplication>(*args)
