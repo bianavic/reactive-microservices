3ed5e3f9de94c343bc3bf2ed850c1fa23e2e656e
bianavic <bianavic@gmail.com>
2021-09-11 14:32:38 -0300

    created a new method to return a list of customers by name

--- src/main/kotlin/com/microservices/studies/CustomerController.kt
+++ src/main/kotlin/com/microservices/studies/CustomerController.kt
@@ -1,11 +1,9 @@
 package com.microservices.studies
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
+import java.util.logging.Filter
 
 @RestController
 class CustomerController {
@@ -16,8 +14,10 @@ class CustomerController {
     @RequestMapping(value = arrayOf("/customer/{id}"), method = arrayOf(RequestMethod.GET))
     fun getCustomer(@PathVariable id : Int) = customers[id]
 
-    // transformed map of customers into a list
+    // filter customer list by name
     @RequestMapping(value = arrayOf("/customers"), method = arrayOf(RequestMethod.GET))
-    fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
-
+    fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
+        customers.filter {
+            it.value.name.contains(nameFilter, true)
+        }.map(Map.Entry<Int, Customer>::value).toList()
 }
