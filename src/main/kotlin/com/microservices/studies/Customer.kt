d426854ccf58009e8061b901a4344c71571b8dfc
bianavic <bianavic@gmail.com>
2021-09-11 21:33:54 -0300

    configured spring globally to not serialized null objecjts

--- src/main/kotlin/com/microservices/studies/Customer.kt
+++ src/main/kotlin/com/microservices/studies/Customer.kt
@@ -2,7 +2,6 @@ package com.microservices.studies
 
 import com.fasterxml.jackson.annotation.JsonInclude
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Customer(var id: Int = 0, var name: String = "", var telephone: Telephone? = null) {
     data class Telephone(var countryCode: String = "", var telephone: String = "")
 }
