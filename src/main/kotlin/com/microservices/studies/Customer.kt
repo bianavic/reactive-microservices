2a6e241bcd69d13ec6272542ff624eef47595e03
bianavic <bianavic@gmail.com>
2021-09-11 22:27:41 -0300

    created a webflux application using netty

--- src/main/kotlin/com/microservices/studies/Customer.kt
+++ src/main/kotlin/com/microservices/studies/Customer.kt
@@ -1,7 +1,5 @@
 package com.microservices.studies
 
-import com.fasterxml.jackson.annotation.JsonInclude
-
 data class Customer(var id: Int = 0, var name: String = "", var telephone: Telephone? = null) {
     data class Telephone(var countryCode: String = "", var telephone: String = "")
 }
--- src/main/kotlin/com/microservices/studies/ErrorHandler.kt
+++ src/main/kotlin/com/microservices/studies/ErrorHandler.kt
@@ -1,11 +1,9 @@
 package com.microservices.studies
 
-import com.fasterxml.jackson.core.JsonParseException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import java.lang.Exception
 import javax.servlet.http.HttpServletRequest
 
 @ControllerAdvice
@@ -15,6 +13,6 @@ class ErrorHandler {
     fun JsonParserExceptionHandler(servletRequest: HttpServletRequest,
                                    exception: Exception): ResponseEntity<ErrorResponse> {
         return ResponseEntity(ErrorResponse("Customer not found", exception.message!! ),
-        HttpStatus.BAD_REQUEST)
+            HttpStatus.BAD_REQUEST)
     }
 }
